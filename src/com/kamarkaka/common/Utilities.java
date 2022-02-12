70179a02024e8655165f286aaaf25de2914349aa
Meng Cao <mencao@microsoft.com>
2022-02-11 20:45:02 -0800

    02/11/2022

--- src/com/kamarkaka/common/Utilities.java
+++ src/com/kamarkaka/common/Utilities.java
@@ -1,6 +1,6 @@
 package com.kamarkaka.common;
 
-import com.kamarkaka.*;
+import com.kamarkaka.stripe.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -42,6 +42,6 @@ public class Utilities {
    }
 
    public static void main(String[] args) {
-      LC0900.run();
+      Points.run();
    }
 }
