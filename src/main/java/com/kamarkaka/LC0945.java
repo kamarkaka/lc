20dcd3e671673f2f32c111c39905cbbceed8334e
Meng Cao <mencao@microsoft.com>
2022-03-21 10:18:15 -0700

    03/21/2022

---
+++ src/main/java/com/kamarkaka/LC0945.java
@@ -0,0 +1,43 @@
+package com.kamarkaka;
+
+import java.util.Arrays;
+
+/***
+ * 945. Minimum Increment to Make Array Unique
+ * You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1.
+ * Return the minimum number of moves to make every value in nums unique.
+ *
+ * Example 1:
+ *    Input: nums = [1,2,2]
+ *    Output: 1
+ *    Explanation: After 1 move, the array could be [1, 2, 3].
+ *
+ * Example 2:
+ *    Input: nums = [3,2,1,2,1,7]
+ *    Output: 6
+ *    Explanation: After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+ *       It can be shown with 5 or less moves that it is impossible for the array to have all unique values.
+ *
+ * Constraints:
+ *    1 <= nums.length <= 10^5
+ *    0 <= nums[i] <= 10^5
+ */
+public class LC0945 {
+   public int minIncrementForUnique(int[] nums) {
+      Arrays.sort(nums);
+      int steps = 0;
+      if (nums.length == 0) return steps;
+      int nextVal = nums[0] + 1;
+
+      for (int i = 1; i < nums.length; i++) {
+         if (nums[i] <= nums[i-1]) {
+            steps += nextVal - nums[i];
+            nums[i] = nextVal;
+            nextVal++;
+         } else {
+            nextVal = Math.max(nums[i] + 1, nextVal + 1);
+         }
+      }
+      return steps;
+   }
+}
