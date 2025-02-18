a46fd5cc7848d1640d77d793f12503aca71fd017
Meng Cao <caomeng1208@gmail.com>
2025-02-18 09:01:51 -0500

    update

--- src/main/java/com/kamarkaka/leetcode/LC0886.java
+++ src/main/java/com/kamarkaka/leetcode/LC0886.java
@@ -1,58 +1,53 @@
 package com.kamarkaka.leetcode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /***
  * 886. Possible Bipartition
- * We want to split a group of n people (labeled from 1 to n) into two groups of any size. Each person may dislike some other people, and they should not go into the same group.
- * Given the integer n and the array dislikes where dislikes[i] = [ai, bi] indicates that the person labeled ai does not like the person labeled bi, return true if it is possible to split everyone into two groups in this way.
- *
+ * We want to split a group of n people (labeled from 1 to n) into two groups of any size. Each person may dislike some
+ * other people, and they should not go into the same group.
+ * Given the integer n and the array dislikes where dislikes[i] = [ai, bi] indicates that the person labeled ai does not
+ * like the person labeled bi, return true if it is possible to split everyone into two groups in this way.
  * Example 1:
- *    Input: n = 4, dislikes = [[1,2],[1,3],[2,4]]
- *    Output: true
- *    Explanation: group1 [1,4] and group2 [2,3].
- *
+ *   Input: n = 4, dislikes = [[1,2],[1,3],[2,4]]
+ *   Output: true
+ *   Explanation: group1 [1,4] and group2 [2,3].
  * Example 2:
- *    Input: n = 3, dislikes = [[1,2],[1,3],[2,3]]
- *    Output: false
- *
+ *   Input: n = 3, dislikes = [[1,2],[1,3],[2,3]]
+ *   Output: false
  * Example 3:
- *    Input: n = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
- *    Output: false
- *
+ *   Input: n = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+ *   Output: false
  * Constraints:
- *    1 <= n <= 2000
- *    0 <= dislikes.length <= 10^4
- *    dislikes[i].length == 2
- *    1 <= dislikes[i][j] <= n
- *    ai < bi
- *    All the pairs of dislikes are unique.
+ *   1 <= n <= 2000
+ *   0 <= dislikes.length <= 10^4
+ *   dislikes[i].length == 2
+ *   1 <= dislikes[i][j] <= n
+ *   ai < bi
+ *   All the pairs of dislikes are unique.
  */
 public class LC0886 {
    public boolean possibleBipartition(int n, int[][] dislikes) {
-      List<Integer>[] map = new List[n+1];
+      int[][] graph = new int[n+1][n+1];
       for (int i = 1; i <= n; i++) {
-         map[i] = new ArrayList<>();
+         Arrays.fill(graph[i], 0);
       }
 
-      for(int[] edge: dislikes){
-         map[edge[0]].add(edge[1]);
-         map[edge[1]].add(edge[0]);
+      for (int[] edge: dislikes) {
+         graph[edge[0]][edge[1]] = 1;
+         graph[edge[1]][edge[0]] = 1;
       }
 
       int[] colors = new int[n+1];
       for (int i = 1; i <= n; i++) {
          if (colors[i] == 0) {
-            if (!bfs(map, colors, i)) return false;
+            if (!bfs(graph, colors, i)) return false;
          }
       }
       return true;
    }
 
-   private boolean bfs(List<Integer>[] map, int[] colors, int i) {
+   private boolean bfs(int[][] graph, int[] colors, int i) {
       int color = 1;
       Queue<Integer> queue = new LinkedList<>();
       queue.add(i);
@@ -60,7 +55,7 @@ public class LC0886 {
       while (!queue.isEmpty()) {
          int cur = queue.poll();
          color = -colors[cur];
-         for (int nxt : map[cur]) {
+         for (int nxt : graph[cur]) {
             if (colors[nxt] == -color) return false;
             if (colors[nxt] == 0) {
                colors[nxt] = color;
